cca868bdcfe9029a7dd5c36097beae8e53e68c0d
Dalton <groff3@etown.edu>
2026-07-30 13:06:46 -0400

    luff gone vro

--- about/MCA.php
+++ about/MCA.php
@@ -20,7 +20,6 @@
         <div class="pfp">
             <img id="Team2026Pic" src="teampics/dog.jpg" alt="2026 Class picture." width=1000 usemap="#classmap">
             <img src="teampics/ironman.jpg">
-            <img src = "teampics/images.jpg">
             <map id="classmap">
                 <area shape="circle" coords="102, 237, 30" href="student_template.php">
             </map>    
