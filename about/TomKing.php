6fde11c1eb68d8b68ae8a97a12abe6e20da9d983
Tom King <king5@etown.edu>
2026-07-30 11:05:19 -0400

    added my page

---
+++ about/TomKing.php
@@ -0,0 +1,23 @@
+<?php
+    $pageName = "Tom King's webpage";
+    require "../includes/functions.php";
+    require "../includes/head.php";
+    require "../includes/navbar.php";
+?>
+
+<body>
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+
+    <main>
+        <section id="tom king webpage">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio">
+                I AM TOM KING
+        </section>
+    </main>
+</body>
+
+<?php require "../includes/footer.php"; ?>
