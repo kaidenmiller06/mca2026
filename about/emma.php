bde7fafdaf6446e76cb18c9a6d83db4e9624739d
Emma Lang <LANG@etown.edu>
2026-07-30 11:14:00 -0400

    added Emma Lang's webpage

---
+++ about/emma.php
@@ -0,0 +1,25 @@
+<?php
+    $pageName = "Emma Lang's Webpage";
+    require "../includes/functions.php";
+    require "../includes/head.php";
+    require "../includes/navbar.php";
+?>
+
+<body>
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+
+    <main>
+        <section id="Emma Lang's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio">
+                My name is Emma and I have three younger brothers. I have taken two programming classes with my school, but plan to take Ap Computer Science and two other 
+                programming classes this upcoming school year. I have three cats and a snake. I enjoy sleeping and reading. I don't know what field I want to go into in the future.
+            </div>
+        </section>
+    </main>
+</body>
+
+<?php require "../includes/footer.php"; ?>
