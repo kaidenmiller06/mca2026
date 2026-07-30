c2f4ae7368a672d7ce3481a71295080d1900c37c
Brennan Boland <boland@etown.edu>
2026-07-30 11:07:36 -0400

    Brennan's webpage

---
+++ about/Brennan.php
@@ -0,0 +1,23 @@
+<?php
+    $pageName = "Jane Doe's Webpage";
+    require "../includes/functions.php";
+    require "../includes/head.php";
+    require "../includes/navbar.php";
+?>
+
+<body>
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Brennan Boland</div>
+            <br>
+            <div id="bio">
+               Hi i am Brennan 
+        </section>
+    </main>
+</body>
+
+<?php require "../includes/footer.php"; ?>
