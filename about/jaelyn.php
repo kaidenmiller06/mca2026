264f6f5d1a2ef359dbc972fb8babd3e0cf2bb202
Jaelyn Thoun <jaelynthoun095@gmail.com>
2026-07-30 11:05:50 -0400

    about/jaelyn.php

---
+++ about/jaelyn.php
@@ -0,0 +1,24 @@
+<?php
+    $pageName = "Jaelyn's Webpage";
+    require "../includes/functions.php";
+    require "../includes/head.php";
+    require "../includes/navbar.php";
+?>
+
+<body>
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Jaelyn</div>
+            <br>
+            <div id="bio">
+                I am Jaelyn. I enjoy coding and wish to pursue computer science as my major in the future.
+            </div>
+        </section>
+    </main>
+</body>
+
+<?php require "../includes/footer.php"; ?>
