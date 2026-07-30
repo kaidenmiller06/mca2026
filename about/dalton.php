138d61dcf8123a4c7c591e5236147b4be8a795ec
Dalton <groff3@etown.edu>
2026-07-30 10:59:35 -0400

    Added Dalton's Webpage

---
+++ about/dalton.php
@@ -0,0 +1,24 @@
+<?php
+    $pageName = "Jane Doe's Webpage";
+    require "../includes/functions.php";
+    require "../includes/head.php";
+    require "../includes/navbar.php";
+?>
+
+<body>
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Dalton Groff</div>
+            <br>
+            <div id="bio">
+                Hello, I am Dalton. I like flying planes and watchmaking
+            </div>
+        </section>
+    </main>
+</body>
+
+<?php require "../includes/footer.php"; ?>
