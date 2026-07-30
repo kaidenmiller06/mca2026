04f23b8b5c1db2533c09c9e6538d5d6184c495ee
Mateo Santos <santos@etown.edu>
2026-07-30 11:02:00 -0400

    added Mateo's webpage

---
+++ about/mateo.php
@@ -0,0 +1,24 @@
+<?php
+    $pageName = "Mateo Santos' Webpage";
+    require "../includes/functions.php";
+    require "../includes/head.php";
+    require "../includes/navbar.php";
+?>
+
+<body>
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Mateo Santos</div>
+            <br>
+            <div id="bio">
+                Hi I am Mateo
+            </div>
+        </section>
+    </main>
+</body>
+
+<?php require "../includes/footer.php"; ?>
