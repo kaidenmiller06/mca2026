f867cadea7dcefb32133a250024d966598820d9b
arvinddulal <arvinddulal7@gmail.com>
2026-07-30 11:22:14 -0400

    added kyleah

--- about/MCA.php
+++ about/MCA.php
@@ -35,6 +35,7 @@
                 <br><a href='colin.php'>Colin</a>
                 <br><a href='Aidan.php'>Aidan</a>
                 <br><a href='Brennan.php'>Brennan</a>
+                <br><a href='kyleah.php'>Kyleah</a>
             </span>
         </div>
     </div>
