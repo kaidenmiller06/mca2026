a4acfdc42adbbb6a47f128a3ae1734ad4cb26cee
megatron <pennell@etown.edu>
2026-07-30 12:42:15 -0400

    My page

---
+++ about/chase.php
@@ -0,0 +1,24 @@
+<?php
+    $pageName = "Jane Doe's Webpage";
+    require "../includes/functions.php";
+    require "../includes/head.php";
+    require "../includes/navbar.php";
+?>
+
+<body>
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Chase Pennell</div>
+            <br>
+            <div id="bio">
+                I am Chase.
+            </div>
+        </section>
+    </main>
+</body>
+
+<?php require "../includes/footer.php"; ?>
