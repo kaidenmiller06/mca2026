1301977531a60541cd9da5a11add89647d11e904
missosigma <mmujgahn@gmail.com>
2026-07-30 13:41:52 -0400

    m

--- about/MCA.php
+++ about/MCA.php
@@ -35,7 +35,9 @@
                 <a href='student_template.php'>Jane Doe</a>
                 <br><a href='ethan.php'>Ethan</a>
                 <br><a href='arvindpage.php'>Arvind</a>
+                <img src="teampics/Images.png">
                 <br><a href='muj.php'>Mujgahn</a>
+                <img src="teampics/Images.png">
                 <br><a href='mateo.php'>Mateo</a>
                 <br><a href='chase.php'>Chase</a>
                 <br><a href='dylan.php'>Dylan</a>
@@ -57,10 +59,24 @@
                 <br><a href='EliRemsberg.php'>Eli</a>
                 <br><a href='emma.php'>Emma</a>
                 <br><a href='km.php'>Kyan</a>
+                <img src="teampics/Images.png">
             </span>
         </div>
     </div>
 </body>
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+<img src="teampics/Images.png">
+
 <script src="oneko.js"></script>
 <script src="../main.js"></script>
 <script src="confetti.js"></script>
