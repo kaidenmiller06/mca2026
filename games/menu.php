6378d6c8649c72581ab5a196ca90f76106ba30ae
missosigma <mmujgahn@gmail.com>
2026-07-30 13:37:18 -0400

    d

--- games/menu.php
+++ games/menu.php
@@ -9,5 +9,6 @@
     <h1>Pong</h1>
     <canvas onmousemove="movePaddle(event)" width="400" height="400" style="background-color: black;"></canvas>
     <script src="pong.js"></script>
+    <img src="../about/teampics/Images.png">
 </body>
 </html>
