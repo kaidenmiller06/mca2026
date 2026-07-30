af1dc3d4cbe09831d97018d1b110eced085a89af
jebididdus <king6@etown.edu>
2026-07-30 11:19:43 -0400

    adding jed king page

---
+++ about/jed king.php
@@ -0,0 +1,24 @@
+<?php
+    $pageName = "Jane Doe's Webpage";
+    require "../includes/functions.php";
+    require "../includes/head.php";
+    require "../includes/navbar.php";
+?>
+
+<body>
+    <div class="buttons">
+        <a href="MCA.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+
+    <main>
+        <section id="Jane Doe's Bio">
+            <div class="bio-title">Jane Doe</div>
+            <br>
+            <div id="bio">
+                I am Jed King
+            </div>
+        </section>
+    </main>
+</body>
+
+<?php require "../includes/footer.php"; ?>
